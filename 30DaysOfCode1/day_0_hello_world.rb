6618b5aff90e261bfbee26c762a138be78c90d92
pamccombs <pa.mccombs1@gmail.com>
2020-08-05 13:50:18 -0400

    add completed code

---
+++ 30DaysOfCode1/day_0_hello_world.rb
@@ -0,0 +1,15 @@
+# Hello World example
+# Problem Statement
+# https://www.hackerrank.com/rest/contests/master/challenges/30-hello-world/download_pdf?language=English
+
+# Complete
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = 'Welcome to 30 Days of Code!'
+
+# Print a string literal saying "Hello, World." to stdout.
+puts 'Hello, World.'
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+puts input_string
