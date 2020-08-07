f19c73f28260f9410bafaf2fab5112e7ccd5f73b
pamccombs <pa.mccombs1@gmail.com>
2020-08-07 13:40:01 -0400

    change to in progress

--- between_two_sets.rb
+++ between_two_sets.rb
@@ -1,4 +1,65 @@
 # Problem Statement
 # https://www.hackerrank.com/rest/contests/master/challenges/between-two-sets/download_pdf?language=English
 
-# Complete
+# In Progress
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+a = [2, 4]
+n = a.count
+b = [16, 32, 96]
+m = b.count
+
+# def getTotalX(a, b)
+#     # Write your code here
+#     a_counter = 0
+#     ##Constraints
+#     if (1<=n && n <=10 && 1<=m && m <=10)
+
+#         a.each do |x|
+#             b.each do |y|
+#                 #Constraints
+#                 if (1 <= x && x <= 100 && 1 <= y && y <= 100)
+
+#                 end
+#             end
+#         end
+#     end
+
+
+# end
+
+# def lcm
+#     i = 0
+#     a = 2
+#     b = 6
+#     arr = []
+
+#     loop do 
+#         i += 1
+#         if (i%b == 0) && (i%a == 0) && ((i*b)%a == 0) && ((a*i)%b == 0)
+#             arr << i
+#         end
+#         break if i == 100
+#     end
+#     return arr
+# end
+
+# puts lcm
+
+arr = [2,6]
+puts arr.lcm
+# puts arr[1]%arr[0]
+
+
+
+
+
+
