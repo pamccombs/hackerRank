8bcecb586dece99b93cd7b1c84d4bfeba51abc2c
pamccombs <pa.mccombs1@gmail.com>
2020-08-07 18:42:31 -0400

    add finished challenge

--- 30DaysOfCode1/day_2_operators.rb
+++ 30DaysOfCode1/day_2_operators.rb
@@ -1,4 +1,23 @@
 # Problem Statement
 # https://www.hackerrank.com/rest/contests/master/challenges/30-operators/download_pdf?language=English
 
+## Completed
 
+
+meal_cost = 12.00
+
+tip_percent = 20
+
+tax_percent = 8
+
+def solve(meal_cost, tip_percent, tax_percent)
+
+    tip = meal_cost * tip_percent/100
+    tax = meal_cost * tax_percent/100
+
+    total = meal_cost + tip + tax
+
+    return total.round
+end
+
+puts solve meal_cost, tip_percent, tax_percent
