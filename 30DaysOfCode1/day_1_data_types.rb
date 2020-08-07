d03475a616cb757787a19fa6e64d5513ba210127
pamccombs <pa.mccombs1@gmail.com>
2020-08-07 13:42:48 -0400

    add completed challenge

---
+++ 30DaysOfCode1/day_1_data_types.rb
@@ -0,0 +1,33 @@
+# Problem Statement
+# https://www.hackerrank.com/rest/contests/master/challenges/30-hello-world/download_pdf?language=English
+
+# input is unknown
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = 0
+b = 0.0
+c = ""
+
+# Read and save an integer, double, and String to your variables.
+# Use gets to read and save input
+
+a = gets.to_i
+b = gets.to_f
+c = gets.to_s
+
+
+# Print the sum of both integer variables on a new line.
+
+puts i + a
+
+# Print the sum of the double variables on a new line.
+
+puts b + d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+puts s + c
+
