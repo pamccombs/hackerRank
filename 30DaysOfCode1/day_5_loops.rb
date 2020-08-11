aa45af7e5806d97e978a95fcc4b671aa72997a65
pamccombs <pa.mccombs1@gmail.com>
2020-08-10 21:22:29 -0400

    add completed challenge

---
+++ 30DaysOfCode1/day_5_loops.rb
@@ -0,0 +1,15 @@
+# Problem Statement
+
+# https://www.hackerrank.com/rest/contests/master/challenges/30-loops/download_pdf?language=English
+
+## Completed
+
+# n = gets.to_i
+n = 2
+
+for i in (1..10) do
+    puts "#{n} x #{i} = #{n*i}"
+end
+
+
+
