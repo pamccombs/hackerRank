d90766af5fa5d34c0fa3dfd34813a41a4a510246
pamccombs <pa.mccombs1@gmail.com>
2020-08-10 20:11:30 -0400

    complete challenge

--- 30DaysOfCode1/day_3_intro_to_conditional_statements.rb
+++ 30DaysOfCode1/day_3_intro_to_conditional_statements.rb
@@ -4,3 +4,31 @@
 
 ## Completed
 
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20, print Not Weird
+
+# N = gets.to_i
+
+N = 3
+
+n = N
+
+
+def weird(n)
+
+    if n.even? && (2..5).member?(n)
+        puts 'Not Weird'
+    elsif n.even? && (6..20).member?(n)
+        puts 'Weird'
+    elsif n.even? && N > 20
+        puts 'Not Weird'
+    elsif n.odd?
+        puts 'Weird'
+    end
+
+end
+
+weird(n)
