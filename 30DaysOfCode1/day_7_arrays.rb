dc39c8167441ddcba85bda56aa9745b63ce95fe6
pamccombs <pa.mccombs1@gmail.com>
2020-08-12 20:21:13 -0400

    add challenge

---
+++ 30DaysOfCode1/day_7_arrays.rb
@@ -0,0 +1,15 @@
+# Problem Statement
+
+# https://www.hackerrank.com/rest/contests/master/challenges/30-arrays/download_pdf?language=English
+
+## Completed
+
+# n = gets.to_i
+
+n = [4564], [9873]
+
+# arr = gets.rstrip.split(' ').map(&:to_i)
+
+# puts arr.join(' ').reverse
+
+puts n
