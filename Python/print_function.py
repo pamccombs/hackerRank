0fb8c11d98d3c49ac209d172037eb311992c15a0
pamccombs <pa.mccombs1@gmail.com>
2020-08-16 04:27:40 -0400

    add completed challenge

---
+++ Python/print_function.py
@@ -0,0 +1,14 @@
+# Problem Statement
+
+# https://www.hackerrank.com/rest/contests/master/challenges/python-print/download_pdf?language=English
+
+## Completed
+
+## Test Values
+
+n = 3
+
+## Solution
+
+for i in range(n):
+        print (i+1, end = '')
