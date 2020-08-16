e55a8424ead2e8d31068c3c3a8a44b4212bd3057
pamccombs <pa.mccombs1@gmail.com>
2020-08-16 04:06:48 -0400

    add completed challenge

---
+++ Python/write_a_function.py
@@ -0,0 +1,28 @@
+
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+
+
+def is_leap(year):
+    leap = True
+
+    # Write your logic here
+    if (year % 4 == 0):
+        if (year % 100 == 0 and year % 400 == 0):
+            return leap
+        elif (year % 100 == 0):
+            return False
+        else:
+            return leap
+    else:
+        return False
+
+
+
+# Sample Value
+year = 1992
+##
+print(is_leap(year))
