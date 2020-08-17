8a75bfad704c7f9f5e30a32fc5db6095829ef410
pamccombs <pa.mccombs1@gmail.com>
2020-08-17 02:40:36 -0400

    add completed challenge

---
+++ 30DaysOfCode1/day_7_arrays.py
@@ -0,0 +1,18 @@
+# Problem Statement
+
+# https://www.hackerrank.com/rest/contests/master/challenges/30-arrays/download_pdf?language=English
+
+## Completed
+
+## Test values
+arr = [4, 5, 6, 4]
+
+## Solution
+
+# n = int(input())
+
+# arr = list(map(int, input().rstrip().split()))
+
+arr.reverse()
+for i in arr:
+    print(i, end=' ')
