d3e547097291ce9a9a1d97d174ac0219d0ec6714
pamccombs <pa.mccombs1@gmail.com>
2020-08-18 03:17:26 -0400

    add sandbox to test new algorithms

---
+++ sandbox.rb
@@ -0,0 +1,24 @@
+arr1 = [1, 2, 3, 8]
+arr2 = [4, 5, 6]
+
+# arr1.zip(arr2).each do |i, j|
+    
+#     break if j == nil
+    
+#     puts i + j
+    
+# end
+# for i, j in arr1.zip(arr2) do
+    
+#     break if j === nil
+    
+#     puts i + j
+    
+# end
+
+a = 1
+b = '1'
+
+puts a == b.to_i
+puts a === b.to_i
+puts a.equal?(b.to_i)
